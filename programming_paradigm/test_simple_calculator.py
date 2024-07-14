f38fbab0f55b7790c59ae969a5fa9e5c8a8e2ee7
eskainah <eskainah@gmail.com>
2024-07-14 23:12:52 +0300

    update multplication test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -17,7 +17,7 @@ class testSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-1,-1),1)
         self.assertEqual(self.calc.subtract(0,1),0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3,2),6)
         self.assertEqual(self.calc.multiply(-1,1),-1)
         self.assertEqual(self.calc.multiply(-1,-1),1)
