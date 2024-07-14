5ba38f6760132275213bee5cfca34a2d2cb3ef83
eskainah <eskainah@gmail.com>
2024-07-14 22:25:20 +0300

    update calculation section

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,6 +4,6 @@ def safe_divide(numerator, denominator):
         d_nom = float(denominator)
         if denominator == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
-        return f"The quotient is: {num/d_nom:.1f}"
+        return f"The quotient is: {num / d_nom:.1f}"
     except ValueError:
         print(f"Error: Please enter numeric values only.")
