5f013a6e9f35f469c642bd437ebfaf414c6f1f47
eskainah <eskainah@gmail.com>
2024-07-14 22:06:29 +0300

    update script

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Your account balance is: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
