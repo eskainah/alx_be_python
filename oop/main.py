882a6c6c3db477ce435e71bf0370771b2cc0ab25
eskainah <eskainah@gmail.com>
2024-07-21 15:19:48 +0300

    update main

--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,17 @@
-from class_static_methods_demo import Calculator
+#from class_static_methods_demo import Calculator
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
 
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+"""
 def main():
     # Using the static method
     sum_result = Calculator.add(10, 5)
@@ -9,5 +21,6 @@ def main():
     product_result = Calculator.multiply(10, 5)
     print(f"The product is: {product_result}")
 
+"""
 if __name__ == "__main__":
     main()
