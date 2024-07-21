7e55fde52a5872ffb62be84df1b4549b3243fb17
eskainah <eskainah@gmail.com>
2024-07-21 14:37:01 +0300

    update addition method

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,8 +1,9 @@
 class Calculator:
     calculation_type = "Arithmetic Operations"
+    
     @staticmethod
     def add(a,b):
-        return a+b
+        return a + b
     
     @classmethod
     def multiply(cls, a, b):
